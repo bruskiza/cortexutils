9573a8c45f2b003bb3f960e4d4fd850e6873eb00
bruskiza <bruce.mcintyre@gmail.com>
2022-06-10 22:04:13 +0100

    fixing

---
+++ cortexutils/metrics2table.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+
+import luigi
+from structlog import get_logger
+import cortexutils.fetcher as fetcher
+from jinja2 import Template
+import json
+from datetime import datetime 
+
+log = get_logger()
+
+def fix_name(host):
+    return host.replace("http://", "").replace("https://", "").replace(":", "_").replace("/", "_")
+
+class GatherMetrics(luigi.WrapperTask):    
+    
+    host = luigi.Parameter()
+    host_type = luigi.Parameter()
+    
+    def requires(self):
+        yield Metrics2Raw(self.host, self.host_type)
+        yield Metrics2Json(self.host, self.host_type)
+        yield Metrics2Table(self.host, self.host_type)
+        yield Metrics2GrafanaDashboard(self.host, self.host_type)
+        yield GetConfigDiff(self.host, self.host_type)
+        yield Metrics2NonZeros(self.host, self.host_type)
+        yield ProcessNonZeros(self.host, self.host_type)
+
+class GetConfigDiff(luigi.Task):
+    
+    host = luigi.Parameter()
+    host_type = luigi.Parameter()
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_{self.host_type}.config.diff")
+    
+    def run(self):
+        endpoint = self.host.replace("/metrics", "/config?mode=diff")
+        log.info(F"Config diff from here: {endpoint}")
+        result = fetcher.get_config_diff(endpoint)
+        with self.output().open("w") as output:
+            output.write(result)
+        
+class Metrics2NonZeros(luigi.Task):
+    """ Separates all metrics out from nulls and non-zeros"""    
+    host = luigi.Parameter()
+    host_type = luigi.Parameter()
+    
+    def requires(self):
+        return Metrics2Raw(self.host, self.host_type)
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_{self.host_type}.raw.non-zero")
+    
+    def run(self):
+        log.info(f"Parsing out all non-zero values ...")
+        log.info(f"last input: {self.input()}")
+        non_zero_metrics = []
+        for line in self.input().open():
+            if "#" in line:
+                continue
+            (metric, value) = fetcher.get_value(line)
+            if metric is not None and value is not None:
+                if metric not in non_zero_metrics:
+                    non_zero_metrics.append(metric)
+        log.info(f"{len(non_zero_metrics)} found. Writing.")
+        with self.output().open("w") as output:
+            output.write("\n".join(non_zero_metrics))
+        
+
+def string_to_dict(string, seperator="_"):
+    data = string.split(seperator)
+    
+
+class Metrics2Geneology(luigi.Task):
+    """ Gets all metrics and separates out geneology family """    
+    host = luigi.Parameter()
+    host_type = luigi.Parameter()
+    
+    def requires(self):
+        return Metrics2Raw(self.host, self.host_type)
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_{self.host_type}.geneology.json")
+    
+    def run(self):
+        log.info(f"Parsing out all non-zero values ...")
+        log.info(f"last input: {self.input()}")
+        non_zero_metrics = []
+        for line in self.input().open():
+            if "#" in line:
+                continue
+            (metric, value) = fetcher.get_value(line)
+            if metric is not None and value is not None:
+                if metric not in non_zero_metrics:
+                    non_zero_metrics.append(metric)
+        log.info(f"{len(non_zero_metrics)} found. Writing.")
+        with self.output().open("w") as output:
+            output.write("\n".join(non_zero_metrics))
+        
+        
+class Metrics2Raw(luigi.Task):
+    """ Downloads ever metric """
+    host = luigi.Parameter()
+    host_type = luigi.Parameter()
+    
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_{self.host_type}.raw")
+    
+    def run(self):
+        result = fetcher.get_metrics(self.host)
+        with self.output().open("w") as output:
+            output.write(result)
+
+
+class ProcessNonZeros(luigi.Task):
+    
+    host = luigi.Parameter()
+    host_type = luigi.Parameter()
+    
+    
+    def requires(self):
+        return Metrics2NonZeros(self.host, self.host_type)
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_{self.host_type}.raw.non-zero.processed")
+    
+    def run(self):
+        log.info(self.input())
+        metrics = self.input().open().read().split("\n")
+        results = {}
+        
+        for metric in metrics:
+            current = metric.split("_")
+            
+        with self.output().open("w") as out:
+            out.write("\n".join(set(metrics)))
+            
+            
+class Metrics2GrafanaDashboard(luigi.Task):
+    host = luigi.Parameter()
+    host_type = luigi.Parameter()
+    
+    
+    def requires(self):
+        return Metrics2Json(self.host, self.host_type)
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_{self.host_type}_dashboard.json")
+    
+    
+    def run(self):
+        file_name = f"results/{fix_name(self.host)}_{self.host_type}.json"
+        log.info(f"File name: {file_name}")
+        data = json.load(open(file_name))
+        panels = []
+        for metric in data['metrics']:
+            log.info(metric.get("metric"))
+            panels.append(json.loads(generate_grafana_panel(metric)))
+
+        # Render out all the metrics
+        t = Template(open(f"templates/grafana_dashboard.jinja2").read())
+        log.info(json.dumps(panels, indent=4))
+        with open(f"results/{fix_name(self.host)}_{self.host_type}_dashboard.json", "w") as output:
+            output.write(t.render(
+                panels=panels,
+                host=self.host,
+                host_type=self.host_type
+            ))
+        
+        
+        
+def generate_grafana_panel(metric):
+    t = Template(open("templates/grafana_panel.jinja2").read())
+    return t.render(
+        description = metric.get("help"),
+        expression = metric.get("metric"),
+        title = metric.get("metric")
+    )
+    
+    
+    
+class Metrics2Json(luigi.Task):
+    
+    host = luigi.Parameter()
+    host_type = luigi.Parameter()
+    
+    def requires(self):
+        Metrics2Raw(self.host, self.host_type)
+    
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_{self.host_type}.json")
+
+    def run(self):
+        summary = fetcher.to_summary(open(f"results/{fix_name(self.host)}_{self.host_type}.raw").read())
+        
+        import json
+        with self.output().open("w") as outfile:
+            outfile.write(json.dumps({"host": self.host, "metrics": summary, "host_type": self.host_type}, indent=4))
+            
+class Metrics2Table(luigi.Task):
+    
+    host = luigi.Parameter()
+    host_type = luigi.Parameter() 
+    
+    
+    def requires(self):
+        return Metrics2Json(self.host, self.host_type)
+    
+
+    def output(self):
+        return luigi.LocalTarget(f'results/{fix_name(self.host)}_{self.host_type}.html')
+
+    def run(self):
+        log.info(f"Parsing the raw file {self.host}")
+        
+        result = json.loads(open(f"results/{fix_name(self.host)}_{self.host_type}.json").read())
+        
+        t = Template(open("templates/metrics_table.jinja2").read())
+        with self.output().open("w") as outfile:
+            outfile.write(t.render(metrics=result.get('metrics'), 
+                                   host=self.host, 
+                                   type=self.host_type,
+                                   date=datetime.now().isoformat()))
+            
+            
+if __name__ == "__main__":
+    luigi.run()
---
+++ cortexutils/metrics_counter.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+
+import luigi
+from structlog import get_logger
+import cortexutils.fetcher as fetcher
+import cortexutils.counter as counter
+from jinja2 import Template
+import json
+from datetime import datetime 
+
+from luigi.contrib import sqla
+
+from sqlalchemy import String
+
+from structlog import get_logger
+
+log = get_logger()
+
+from prometheus_api_client import PrometheusConnect
+
+log = get_logger()
+
+def fix_name(host):
+    """Make a file-friendly name of the host. """
+    return host.replace("http://", "").replace("https://", "").replace(":", "_").replace("/", "_")
+
+
+class GetAllMetrics(luigi.Task):
+    """ Connects to a prom and gets all the metrics ands saves it to a file. """
+    host = luigi.Parameter()
+        
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_all_metrics.txt")
+    
+    def run(self):
+        prom = PrometheusConnect(url=self.host)
+        with self.output().open("w") as out:
+            out.write("\n".join(prom.all_metrics()))
+        
+
+def get_metric(metric, prom):
+    """Gets the metric from a prom provided. """
+    data = prom.custom_query(query=metric)
+    return data
+
+
+class Metrics2DB(sqla.CopyToTable):
+    """ Attempting to test out the CopyToTable task. 
+    
+    This will: 
+    
+    - iterate through every metric
+    - Find the instances
+    - Write it to a metrics_senders table
+    
+    """
+    
+    
+    host = luigi.Parameter()
+    
+    connection_string = "sqlite:///metrics.db"
+    table = "metrics_senders"
+    
+    columns = [
+        (["id", String(50)], {"primary_key": True}), 
+        (["source", String(255)], {}),
+        (["sender", String(255)], {}), 
+        (["job", String(255)], {}), 
+        (["metric", String(255)], {})
+    ]
+    
+    
+    def requires(self):
+        return FetchEveryMetric(self.host)    
+    
+
+    def prepare(self):
+        file_name = f"{fix_name(self.requires().host)}"
+        # log.info(f"Setting new table name: {file_name}")
+        # self.table = file_name
+        
+
+        log.info(self.requires().output().open())
+    
+        data = json.load(self.requires().output().open())
+        # for metric in data["metrics"]:
+            # instances = data["metrics"].get(metric)
+            # log.info(f"{metric}: {len(instances)}")
+        #     # for instance in data["metrics"].get(metric):
+        results = []
+        count = 0
+        for metric in data.get("metrics"):
+            # log.info(metric)
+            
+            current_metrics = data.get("metrics").get(metric)
+            for instance in current_metrics:
+                # log.info(instance)
+                cm = instance.get("metric")
+                current_instance = cm.get("instance")
+                current_job = cm.get("job")
+                results.append((count, self.requires().host, current_instance, current_job, metric))
+                count+=1
+        
+        # log.info(results)
+        return results
+    
+    def rows(self):
+        for row in self.prepare():
+            yield row
+                
+    
+    
+    
+class SummarizeEveryMetric(luigi.Task):
+    """Writes out a summary of every metric"""
+    host = luigi.Parameter()
+    
+    def requires(self):
+        return FetchEveryMetric(self.host)    
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_every_metric_summary.json")
+    
+    def run(self):
+        data = json.load(self.input().open())
+        summary = {
+            "null": len(data.get("null")),
+            "summary": len(data.get("metrics"))
+        }
+        with self.output().open("w") as out:
+            out.write(json.dumps(summary, indent=4))
+            
+class instances2timeseries(luigi.Task):
+    """Writes out a summary of every metric"""
+    host = luigi.Parameter()
+    
+    def requires(self):
+        return FetchEveryMetric(self.host)    
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_instances_with_timeseries.json")
+    
+    def run(self):
+        data = json.load(self.input().open())
+        metrics = data.get('metrics')
+        hosts = {}
+        for metric in metrics:
+            for instance in counter.get_instances(metrics.get(metric)):
+                if instance not in hosts:
+                    hosts[instance] = []
+                if metric not in hosts[instance]:
+                    hosts[instance].append(metric)
+                    
+        with self.output().open("w") as out:
+            out.write(json.dumps(hosts, indent=4))
+
+
+class instances2jobs(luigi.Task):
+    """Writes out a summary of every metric"""
+    host = luigi.Parameter()
+    
+    def requires(self):
+        return FetchEveryMetric(self.host)    
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_instances_with_jobs.json")
+    
+    def run(self):
+        data = json.load(self.input().open())
+        metrics = data.get('metrics')
+        hosts = {}
+        for metric in metrics:
+            for instance in counter.get_jobs(metrics.get(metric)):
+                if instance not in hosts:
+                    hosts[instance] = []
+                if metric not in hosts[instance]:
+                    hosts[instance].append(metric)
+                    
+        with self.output().open("w") as out:
+            out.write(json.dumps(hosts, indent=4))
+        
+class process_instances(luigi.Task):
+    """Writes out a summary of every metric"""
+    host = luigi.Parameter()
+    
+    def requires(self):
+        return FetchEveryMetric(self.host)    
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_instances_with_jobs_with_series.json")
+    
+    def run(self):
+        data = json.load(self.input().open())
+        result = counter.process_instances(data)            
+        with self.output().open("w") as out:
+            out.write(json.dumps(result, indent=4))
+
+class process_jobs(luigi.Task):
+    """Writes out a summary of every metric"""
+    host = luigi.Parameter()
+    
+    def requires(self):
+        return FetchEveryMetric(self.host)    
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_jobs_with_instances_with_series.json")
+    
+    def run(self):
+        data = json.load(self.input().open())
+        result = counter.process_instances(data)            
+        with self.output().open("w") as out:
+            out.write(json.dumps(result, indent=4))
+
+
+class FetchEveryMetric(luigi.Task):
+    """ Fetches every metric for a given host """
+    host = luigi.Parameter()
+            
+    def requires(self):
+        return GetAllMetrics(self.host)
+    
+    def output(self):
+        return luigi.LocalTarget(f"results/{fix_name(self.host)}_every_metric.json")
+    
+    def run(self):
+        prom = PrometheusConnect(url=self.host)
+        null_metrics = []
+        metrics = {}
+        for line in self.input().open().read().split("\n"):
+            data = get_metric(line, prom)
+            if data is None or data == []:
+                null_metrics.append(line)
+            else:
+                metrics[line] = data
+                
+        with self.output().open("w") as out:
+            out.write(json.dumps({"metrics": metrics, "null": null_metrics, }, indent=4))
+                
+            
+            
+if __name__ == "__main__":
+    luigi.run()
